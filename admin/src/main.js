c53f0a7d2ed37a23c78243575c8c687f551b7c4a
lsc <lsc_1217@163.com>
2021-05-11 23:21:10 +0800

    通过在main.js中加入Vue.mixin 修复了通过el-upload不能上传文件的问题

--- admin/src/main.js
+++ admin/src/main.js
@@ -6,6 +6,21 @@ import './style.css'
 import http from './http'
 Vue.prototype.$http = http
 
+Vue.mixin({
+  methods: {
+    getAuthHeaders() {
+      return {
+        Authorization: `Bearer ${localStorage.token || ''}`
+      }
+    }
+  },
+  computed: {
+    uploadUrl() {
+      return this.$http.defaults.baseURL + '/upload'
+    }
+  }
+})
+
 Vue.config.productionTip = false
 
 new Vue({
