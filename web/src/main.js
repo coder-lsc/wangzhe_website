502d1e42dc06d8d8f907194cc51b11274f381326
lsc <lsc_1217@163.com>
2021-05-19 18:43:12 +0800

    scss变量名分离、增加首页轮播图(swiper)

--- web/src/main.js
+++ web/src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/swiper-bundle.css'
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
+
 Vue.config.productionTip = false
 
-import './style.scss'
+import './assets/scss/style.scss'
 
 new Vue({
   router,
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,15 +1,47 @@
 <template>
   <div>
-    首页
+    <swiper ref="mySwiper" :options="swiperOptions">
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg">
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-2" 
+      slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+      return {
+        swiperOptions: {
+          pagination: {
+            el: '.pagination-home'
+          },
+          // Some Swiper option/callback...
+        }
+      }
+    },
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../assets/scss/_variables.scss';
 
+  .pagination-home {
+    .swiper-pagination-bullet {
+      opacity: 1;
+      border-radius: 0.1538rem;
+      background: #fff;
+      &.swiper-pagination-bullet-active {
+        background: map-get($colors, 'info');
+      }
+    }
+  }
 </style>
